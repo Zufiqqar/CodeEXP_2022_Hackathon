7fcb63a200c5112a517f9615f4dbfed83ff01ebc
Benjamin Toh <frogggftw@gmail.com>
2022-06-13 16:35:01 +0800

    create and login

--- nova/pages/index.jsx
+++ nova/pages/index.jsx
@@ -2,12 +2,11 @@ import { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Login from "./login";
+import Create from "./Create";
 
 const Home = () => {
   return (
-    <div>
-      <Login />
-    </div>
+      <Create/>
   );
 };
 
