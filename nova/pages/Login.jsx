3db796ce0b802cfac5002236f0fc8fe62b07f6fc
Benjamin Toh <frogggftw@gmail.com>
2022-06-13 17:06:05 +0800

    test endpoints

--- nova/pages/Login.jsx
+++ nova/pages/Login.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Create from "./Create";
+import {Routes, Route, useNavigate} from 'react-router-dom';
 
 function Login() {
   return (
@@ -45,7 +47,7 @@ function Login() {
                     </form>
                 </div>
                 <div class="flex items-center justify-center mt-6">
-                    <a href="#" target="_blank" class="inline-flex items-center text-xs font-thin text-center text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white">
+                    <a href="Create" target="_blank" class="inline-flex items-center text-xs font-thin text-center text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white">
                         <span class="ml-2">
                             You don&#x27;t have an account?
                         </span>
