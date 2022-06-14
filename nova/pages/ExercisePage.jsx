11e76471c76c35d21ee5f2d84d001c0728fe6e5d
Sky Livingston <Skypad123@gmail.com>
2022-06-14 16:41:57 +0800

    complete timer + instruction combo

--- nova/pages/ExercisePage.jsx
+++ nova/pages/ExercisePage.jsx
@@ -1,7 +1,7 @@
 
 import useSWR from "swr";
 import React, { useEffect, useState } from "react";
-
+import { floor } from 'mathjs'
 
 /* format of JSON */
 /*
@@ -27,20 +27,21 @@ function ExercisePage(props) {
   //const [stage, useStage] = useState(data[0].id)
   const [count, setCount]= useState(0)
 
-  return <div class=" w-screen h-screen flex flex-1 flex-col justify-center align-center ">
-    <p class="text-center">test</p>
-    <ActivityCard 
-    title={"test".concat(count.toString())}
-    content_text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie parturient et sem ipsum volutpat vel. Natoque sem et aliquam mauris egestas quam volutpat viverra. In pretium nec senectus erat. Et malesuada lobortis."
-    id={count}
-    next_fun={()=>{handleNext()}}
-    ></ActivityCard>
 
-    <TimerCard min={1} sec={0}></TimerCard>
+  return <div class=" w-screen h-screen flex flex-1 flex-col justify-center align-center ">
+      <TimedActivityCard
+      title={": test"}
+      content_text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie parturient et sem ipsum volutpat vel. Natoque sem et aliquam mauris egestas quam volutpat viverra. In pretium nec senectus erat. Et malesuada lobortis."
+      id={count}
+      next_fun={()=>{handleNext()}}
+      time={10}
+      ></TimedActivityCard>
     </div>
   
   function handleNext(){
-    setCount(count+1)
+    setCount((count)=>{
+      return count+1
+    })
   }
 
 
@@ -49,8 +50,40 @@ function ExercisePage(props) {
 export default ExercisePage;
 
 
+
+function TimedActivityCard(props){
+
+
+  function handleNext(){
+    //reset to init page
+    setState("Pre-Activity")
+    props.next_fun()
+  }
+  const [state, setState]= useState("Pre-Activity")
+  function displayCards() {
+    switch(state){
+      case "Pre-Activity":
+        return <ActivityCard id={props.id} title={props.title} content_text={props.content_text} next_fun={()=>{setState("Doing-Activity")}}></ActivityCard>
+        break;
+      case "Doing-Activity":
+        return <TimerCard time ={props.time} next_fun={handleNext}></TimerCard>
+        break;
+      case "Complete-Activity":
+        return <div></div>
+        break;
+    }
+  }
+  return <div>
+    {
+    displayCards()
+    }
+    </div>
+}
+
+
 function ActivityCard(props){
   return <div class="max-w-2xl mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 max-h-min">
+  {/* auto play video here */}
   <img class="object-cover w-full h-64" 
   src="https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" 
   alt="Article"/>
@@ -58,7 +91,7 @@ function ActivityCard(props){
   <div class="p-6">
       <div>
           {/* <span class="text-xs font-medium text-blue-600 uppercase dark:text-blue-400">Product</span> */}
-          <a href="#" class="block mt-2 text-2xl font-semibold text-gray-800 transition-colors duration-200 transform dark:text-white hover:text-gray-600 hover:underline">{props.title}</a>
+          <a href="#" class="block mt-2 text-2xl font-semibold text-gray-800 transition-colors duration-200 transform dark:text-white hover:text-gray-600 hover:underline">{props.id.toString().concat(props.title) }</a>
           <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">{props.content_text}</p>
       </div>
 
@@ -67,17 +100,9 @@ function ActivityCard(props){
 
             <button class="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" 
               onClick={()=>props.next_fun()}>
-                  Next Activity
+                  Start Activity
             </button>
 
-
-              {/* <div class="flex items-center">
-                  <img class="object-cover h-10 rounded-full" 
-                  src="https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=48&q=60" 
-                  alt="Avatar"/>
-                  <a href="#" class="mx-2 font-semibold text-gray-700 dark:text-gray-200">Jone Doe</a> 
-              </div>
-              <span class="mx-1 text-xs text-gray-600 dark:text-gray-300">21 SEP 2015</span> */}
           </div>
       </div>
   </div>
@@ -85,8 +110,8 @@ function ActivityCard(props){
 }
 
 function TimerCard(props){
-  var minLimit = props.min
-  var secLimit = props.sec 
+  var minLimit = Math.floor(props.time / 60)
+  var secLimit = props.time % 60
 
   const [state,setState] = useState("Pre-Run")
   const [minsec,setMinsec] = useState([0,0])
@@ -99,7 +124,6 @@ function TimerCard(props){
       if (minsec[1]<59) {return [minsec[0],minsec[1]+1]}
       else if(minsec[1]==59){return [minsec[0]+1,minsec[1]-59]}
     })
-
   }
 
   function endTimer(){
@@ -121,19 +145,23 @@ function TimerCard(props){
     switch(state){
       case "Pre-Run": 
         return <div>
-          <button class="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" 
+          <button class="mt-2 px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" 
           onClick={()=>startTimer()}>
               Start Timer
           </button>
         </div> 
         break;
       case "Running":
-        return <div></div>
+        return <div>
+          <button class="mt-2 px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" >
+            Jiayou!
+          </button>
+        </div>
         break; 
       case "Post-Run":
-        return <button class="px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" 
+        return <button class="mt-2 px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80" 
         onClick={()=>props.next_fun()}>
-            Next Activity
+            Next
         </button>
         break;
     }
